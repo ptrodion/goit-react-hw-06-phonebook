d0d6cf9375eaa900150e7037a763f6c6c0102aed
ptrodion <rdnpetrenko@gmail.com>
2023-10-02 21:40:31 +0100

    add selectors

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,15 @@ import { ContactForm } from './FormContacts/ContactsForm';
 import { Filter } from './Filter/Filter';
 import { ContactsList } from './ContactsList/ContactsList';
 import { useSelector } from 'react-redux';
-import { useMemo } from 'react';
+import { contactsSelector, filterSelector } from 'redux/selectors';
 
 export const App = () => {
-  const contacts = useSelector(state => state.changeContacts.items);
-  const filter = useSelector(state => state.filter);
+  const contacts = useSelector(contactsSelector);
+  const filter = useSelector(filterSelector);
 
-  const filteredContacts = useMemo(() => {
-    return contacts.filter(contact =>
-      contact.name.toUpperCase().includes(filter.toUpperCase())
-    );
-  }, [contacts, filter]);
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toUpperCase().includes(filter.toUpperCase())
+  );
 
   return (
     <Layout>
